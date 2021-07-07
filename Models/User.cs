cc413bc94a6ec4a675a8c48bd3c50238121b99f7
DANA-HYEON <dada4202@gmail.com>
2021-07-07 16:51:20 +0900

    초기 커밋.

---
+++ Models/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotteokChatBot.Models
+{
+    public class User
+    {
+        [Key]
+        public long User_Id { get; set; } //트위치 external key
+
+        [Required]
+        public string Refresh_Token { get; set; } //트위치 refresh token
+        
+        [Required]
+        public string User_login{get;set;} //접속한 스트리머의 channel name
+        
+        public int Blacklist_Status { get; set; } //금칙어 사용유무
+        
+        public int Auto_Greet { get; set; } //자동인사 사용유무
+        
+        public DateTime RegDate { get; set; } //호떡 사용 시작일자
+    }
+}
