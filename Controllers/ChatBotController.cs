4720b14b63de41a959d22c44abc6e32a18baa852
DANA-HYEON <dada4202@gmail.com>
2021-07-09 16:07:47 +0900

    thread 수정

--- Controllers/ChatBotController.cs
+++ Controllers/ChatBotController.cs
@@ -32,11 +32,9 @@ namespace HotteokChatBot.Controllers
 
         public IActionResult ChatBotStart()
         {
-            TwitchBot a = ChatBot_Thread();
-            TwitchBot b = ChatBot_Thread2();
 
-            Thread ChatBot = new Thread(new ThreadStart(a.BotStart));
-            Thread ChatBot2 = new Thread(new ThreadStart(b.BotStart));
+            Thread ChatBot = new Thread(new ThreadStart(ChatBot_Thread));
+            Thread ChatBot2 = new Thread(new ThreadStart(ChatBot_Thread2));
             ChatBot.IsBackground = true; //메인 쓰레드가 종료되면 백그라운드 쓰레드가 작업을 중지하고 종료한다.
             ChatBot.Start();
             ChatBot2.Start();
@@ -61,7 +59,7 @@ namespace HotteokChatBot.Controllers
         /// <summary>
         /// 채팅봇 실행
         /// </summary>
-        public TwitchBot ChatBot_Thread()
+        public void ChatBot_Thread()
         {
             string _broadcasterName;
 
@@ -75,21 +73,17 @@ namespace HotteokChatBot.Controllers
             string _twitchOAuth = "oauth:rmme7sswerrdkrt81140rlfjvra8l6"; //투나빵셔틀(호떡) access_token
 
             TwitchBotService = new TwitchBot(_botName, _broadcasterName, _twitchOAuth);
-
-            return TwitchBotService;
-            //TwitchBotService.BotStart();
+            TwitchBotService.BotStart();
         }
 
-        public TwitchBot ChatBot_Thread2()
+        public void ChatBot_Thread2()
         {
             string _botName = "Hotteok"; //채팅봇 이름
             string _broadcasterName = "wlh9566"; //스트리머 채널 이름
             string _twitchOAuth = "oauth:rmme7sswerrdkrt81140rlfjvra8l6"; //투나빵셔틀(호떡) access_token
 
             TwitchBotService = new TwitchBot(_botName, _broadcasterName, _twitchOAuth);
-
-            return TwitchBotService;
-            //TwitchBotService.BotStart();
+            TwitchBotService.BotStart();
         }
     }
 }
